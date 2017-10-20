42e2f43ee0c34a24303cbb1199c9256e21aeff90
joshua <joshua@localhost.localdomain>
2017-10-19 18:10:30 -0700

    update Menu and functions

--- HDDupdate.sh
+++ HDDupdate.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 echo 'HDD Update Script'
+##### Constants
 
+DRIVEFWPATH="$(pwd)"
+FILEPATH="$(pwd)"
 
-rm report.txt
+##### Functions
 
+update_all(){
 lsscsi -g | grep "disk" > disk.txt
 awk '{print $4" "$5"\t"$6"\t"$7"\t"$8}' disk.txt > data.txt
 column -t data.txt > dataparse.txt
+}
 
-file="/home/joshua/Desktop/HDDupdate/dataparse.txt"
+drive_sort(){
+file="${FILEPATH}""/dataparse.txt"
 while IFS="  " read -r f1 f2 f3 f4 f5  
 do 
 	echo "$f2" "$f3" "$f5" | grep "/dev/sg" >> output.txt
@@ -17,14 +23,17 @@ do
 done <"$file"
 
 
-file="/home/joshua/Desktop/HDDupdate/dataparse.txt"
+file="${FILEPATH}""/dataparse.txt"
 while IFS="  " read -r f1 f2 f3 f4 f5  
 do 
 	echo "$f1" "$f2" "$f4" | grep "/dev/sg" >> output.txt
     
 done <"$file"
 column -t output.txt > updater.txt
+}
 
+
+drive_update(){
 file="/home/joshua/Desktop/HDDupdate/updater.txt"
 while IFS=" " read -r f1 f2 f3  
 do 
@@ -34,11 +43,10 @@ do
 	if [ "${COMPARE}" != "$f2" ]; then
 		OUTPUT="$(awk '{print $3 }' fwtoload.txt)"
 		FILESIZE="$(ls -l | grep "${OUTPUT}" | awk '{print $5}')"
-		FILEPATH="$(pwd)"
-		sg_write_buffer -v --in="${FILEPATH}"/"${OUTPUT}" --length="${FILESIZE}" --mode=5 "$f3" 
+		sg_write_buffer -v --in="${DRIVEFWPATH}"/"${OUTPUT}" --length="${FILESIZE}" --mode=5 "$f3" 
 		STATUS="$(echo $?)"
 		if [ "${STATUS}" != "0" ]; then
-			sg_write_buffer -v --in="${FILEPATH}"/"${OUTPUT}" --length="${FILESIZE}" --mode=7 --offset=0 "$f3"
+			sg_write_buffer -v --in="${DRIVEFWPATH}"/"${OUTPUT}" --length="${FILESIZE}" --mode=7 --offset=0 "$f3"
 			STATUS="$(echo $?)"
 			if [ "${STATUS}" != "0" ]; then
 				echo "$f1 $f2 $f3 FW not updated." >> report.txt
@@ -57,6 +65,41 @@ do
 	fi
 
 done <"$file"
+}
+
+
+##### Main 
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -a|--all)
+    update_all()
+    ;;
+    -m|--model)
+    DRIVEMODEL="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--firmware)
+    FIRMWARE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 rm output.txt
 rm dataparse.txt

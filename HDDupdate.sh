45e5bea834c6aa0b8d51c825fe9dba03160bf747
joshua <joshua@localhost.localdomain>
2017-10-20 17:50:37 -0700

    Function/Main tests

--- HDDupdate.sh
+++ HDDupdate.sh
@@ -8,7 +8,7 @@ FILEPATH="$(pwd)"
 
 ##### Functions
 
-help(){
+helper(){
 exit 0
 }
 
@@ -16,22 +16,25 @@ update_all(){
 lsscsi -g | grep "disk" > disk.txt
 awk '{print $4" "$5"\t"$6"\t"$7"\t"$8}' disk.txt > data.txt
 column -t data.txt > dataparse.txt
+echo "pop"
 }
 
 update_model(){
 lsscsi -g | grep "${DRIVEMODEL}" > disk.txt
 awk '{print $4" "$5"\t"$6"\t"$7"\t"$8}' disk.txt > data.txt
 column -t data.txt > dataparse.txt
+echo "push"
 }
 
 update_drive(){
 lsscsi -g | grep "${SPECIFICDRIVE}" > disk.txt
 awk '{print $4" "$5"\t"$6"\t"$7"\t"$8}' disk.txt > data.txt
 column -t data.txt > dataparse.txt
+echo "fizz"
 }
 
 drive_sort(){
-file="${FILEPATH}""/dataparse.txt"
+file="${FILEPATH}"/"dataparse.txt"
 while IFS="  " read -r f1 f2 f3 f4 f5  
 do 
 	echo "$f2" "$f3" "$f5" | grep "/dev/sg" >> output.txt
@@ -39,7 +42,7 @@ do
 done <"$file"
 
 
-file="${FILEPATH}""/dataparse.txt"
+file="${FILEPATH}"/"dataparse.txt"
 while IFS="  " read -r f1 f2 f3 f4 f5  
 do 
 	echo "$f1" "$f2" "$f4" | grep "/dev/sg" >> output.txt
@@ -55,12 +58,16 @@ rm disk.txt
 
 
 drive_update(){
-file="/home/joshua/Desktop/HDDupdate/updater.txt"
+file="${FILEPATH}"/"updater.txt"
 while IFS=" " read -r f1 f2 f3  
 do 
 	
 	cat fwlist.txt | grep "$f1" > fwtoload.txt
+	if [ "${FIRMWARE}" != "" ]; then 
+	COMPARE="${FIRMWARE}"
+	else
 	COMPARE="$(awk '{print $2 }' fwtoload.txt)"
+	fi
 	if [ "${COMPARE}" != "$f2" ]; then
 		OUTPUT="$(awk '{print $3 }' fwtoload.txt)"
 		FILESIZE="$(ls -l | grep "${OUTPUT}" | awk '{print $5}')"
@@ -123,7 +130,7 @@ case $key in
     shift
     ;;
     *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
+    NOARGUMENT="YES"
     shift # past argument
     ;;
 esac
@@ -131,19 +138,27 @@ done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
 if [ "${SPECIFICDRIVE}" = "YES" ]; then
-	update_drive()
-else if [ "${SPECIFICMODEL}" = "YES" ]; then
-	update_model()
-else if [ "${UPDATEALL}" = "YES" ]; then
-	update_all()
-else if [ "${OPENHELP}" = "YES" ]; then
-	help()
-else
-	help()
+	update_drive
+	echo " updating drive"
+fi
+if [ "${SPECIFICMODEL}" = "YES" ]; then
+	update_model
+	echo " updating drive model "
+fi
+if [ "${UPDATEALL}" = "YES" ]; then
+	update_all
+	echo " updating all drives"
+fi
+if [ "${OPENHELP}" = "YES" ]; then
+	helper
+fi
+if [ "${NOARGUMENT}" = "YES" ]; then
+	helper
 fi
 
-drive_sort()
-drive_update()
+
+drive_sort
+drive_update
 
 rm fwtoload.txt
 rm updater.txt
